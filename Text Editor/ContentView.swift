4406ade0710cf077e0340f5a16cae01a05ea0534
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-04 15:10:03 -0500

    Initial Commit

---
+++ Text Editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Text Editor
+//
+//  Created by Andy Dobbs on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Text Editor/Text_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_EditorApp.swift
+//  Text Editor
+//
+//  Created by Andy Dobbs on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Text_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
