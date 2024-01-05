cf8037333a55b1e899e703954dafcd02b487e5f7
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-04 19:52:41 -0500

    Implement button actions

--- Text Editor/ContentView.swift
+++ Text Editor/ContentView.swift
@@ -10,14 +10,46 @@ import SwiftUI
 struct ContentView: View {
     
     @State var userText = "Test this out"
+    @State var capMode = 1
     
     var body: some View {
         VStack {
-            Text(userText)
-                .font(.largeTitle)
+            if capMode == 1 {
+                Text(userText.uppercased())
+                    .font(.largeTitle)
+            } else if capMode == 2 {
+                Text(userText.capitalized)
+                    .font(.largeTitle)
+            } else if capMode == 3 {
+                Text(userText.lowercased())
+                    .font(.largeTitle)
+            }
+            
             TextField("Enter text here", text: $userText)
-                .padding()
+                .padding(.vertical)
+            
+            HStack {
+                Button("ALL CAPS") {
+                    capMode = 1
+                }
+                Button("First Letter") {
+                    capMode = 2
+                }
+                Button("lowercase") {
+                    capMode = 3
+                }
+            }
+            
+            Button("Copy Text") {
+                copyToClipboard(text: userText)
+            }
+            .padding(.vertical)
         }
+        .padding()
+    }
+    
+    func copyToClipboard(text: String) {
+        UIPasteboard.general.string = text
     }
 }
 
